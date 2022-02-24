f1220942e76ba3ad65094df4cd5eefa5bce3f9b6
Qian Han <kategreat1@hotmail.com>
2022-02-24 15:29:01 +1300

    fixed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const userRoutes = require('./routes/users')
 const petsRoutes = require('./routes/pets')
-const humanRoutes = require('./routes/human')
+const humanRoutes = require('./routes/humans')
 const matchesRoutes = require('./routes/matches')
 const server = express()
 
@@ -12,8 +12,8 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/pets', petsRoutes)
-server.use('/api/v1/pets', humanRoutes)
-server.use('/api/v1/pets', matchesRoutes)
+server.use('/api/v1/humans', humanRoutes)
+server.use('/api/v1/matches', matchesRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))

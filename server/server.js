481a8ef988dc1eb23edd85dfd2a63bbc49eb5561
Qian Han <kategreat1@hotmail.com>
2022-02-24 15:22:38 +1300

    routes done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const express = require('express')
 
-const fruitRoutes = require('./routes/fruits')
 const userRoutes = require('./routes/users')
+const petsRoutes = require('./routes/pets')
+const humanRoutes = require('./routes/human')
+const matchesRoutes = require('./routes/matches')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/users', userRoutes)
+server.use('/api/v1/pets', petsRoutes)
+server.use('/api/v1/pets', humanRoutes)
+server.use('/api/v1/pets', matchesRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))

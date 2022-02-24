5cfe843346dd14dd33e1a3ff9ecba0429a0444c7
Qian Han <kategreat1@hotmail.com>
2022-02-24 15:53:25 +1300

    fruit back in

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const path = require('path')
 const express = require('express')
 
+const fruitRoutes = require('./routes/fruits')
 const userRoutes = require('./routes/users')
-const petsRoutes = require('./routes/pets')
-const humanRoutes = require('./routes/humans')
-const matchesRoutes = require('./routes/matches')
+// const petsRoutes = require('./routes/pets')
+// const humanRoutes = require('./routes/humans')
+// const matchesRoutes = require('./routes/matches')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
+server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/users', userRoutes)
-server.use('/api/v1/pets', petsRoutes)
-server.use('/api/v1/humans', humanRoutes)
-server.use('/api/v1/matches', matchesRoutes)
+// server.use('/api/v1/pets', petsRoutes)
+// server.use('/api/v1/humans', humanRoutes)
+// server.use('/api/v1/matches', matchesRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))

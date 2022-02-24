76202a87f1b9280ec7403ba29c09f9a739949c2d
tarek-chakra <tarek.s.abouchakra@gmail.com>
2022-02-24 15:17:08 +1300

    create prelim chat app

---
+++ client/components/Chat.jsx
@@ -0,0 +1,10 @@
+import { ChatEngine } from 'react-chat-engine'
+import React from 'react'
+
+function Chat () {
+  return (<ChatEngine
+    height='100vh' projectID='f2cbd1f4-72ae-4213-a08a-e4d0702bdb04' userName='Tarek' userSecret='test123'
+  />)
+}
+
+export default Chat

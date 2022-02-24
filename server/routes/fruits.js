09b2f706f2b773da706457314d34094c637f2ede
Qian Han <kategreat1@hotmail.com>
2022-02-24 14:52:47 +1300

    server

--- server/routes/fruits.js
+++ server/routes/fruits.js
@@ -1,36 +1,49 @@
 const express = require('express')
 // TODO: import checkJwt
-const db = require('../db/fruits')
+const db = require('../db/pets')
 
 const router = express.Router()
 
 module.exports = router
 
-// A public endpoint that anyone can access
-// GET /api/v1/fruits
+// homepage for sign in
+// GET /api/v1/home
+
 router.get('/', async (req, res) => {
   try {
-    const fruits = await db.getFruits()
-    res.json({ fruits })
+    const home = await db.gethome()
+    res.json({ home })
   } catch (err) {
     console.error(err)
     res.status(500).send(err.message)
   }
 })
 
-// use checkJwt as middleware
-// POST /api/v1/fruits
+// POST /api/v1/signin
 router.post('/', async (req, res) => {
-  const { fruit } = req.body
+  const { pet } = req.body
   const auth0Id = req.user?.sub
-  const newFruit = {
+  const newpet = {
     added_by_user: auth0Id,
-    name: fruit.name,
-    calories: fruit.calories
+    name: pet.name,
+    calories: pet.calories
   }
   try {
-    const fruits = await db.addFruit(newFruit)
-    res.json({ fruits })
+    const pets = await db.addpet(newpet)
+    res.json({ pets })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err.message)
+  }
+})
+
+// A route to direct you to the pets page after you sign in
+// GET /api/v1/pets
+
+router.get('/', async (req, res) => {
+  try {
+    const pets = await db.getpets()
+    res.json({ pets })
   } catch (err) {
     console.error(err)
     res.status(500).send(err.message)
@@ -38,24 +51,24 @@ router.post('/', async (req, res) => {
 })
 
 // use checkJwt as middleware
-// PUT /api/v1/fruits
+// PUT /api/v1/pets
 router.put('/', async (req, res) => {
-  const { fruit } = req.body
+  const { pet } = req.body
   const auth0Id = req.user?.sub
-  const fruitToUpdate = {
-    id: fruit.id,
+  const petToUpdate = {
+    id: pet.id,
     added_by_user: auth0Id,
-    name: fruit.name,
-    calories: fruit.calories
+    name: pet.name,
+    calories: pet.calories
   }
   try {
-    const fruits = await db.updateFruit(fruitToUpdate, auth0Id)
-    res.json({ fruits })
+    const pets = await db.updatepet(petToUpdate, auth0Id)
+    res.json({ pets })
   } catch (err) {
     console.error(err)
     if (err.message === 'Unauthorized') {
       return res.status(403).send(
-        'Unauthorized: Only the user who added the fruit may update it'
+        'Unauthorized: Only the user who added the pet may update it'
       )
     }
     res.status(500).send(err.message)
@@ -63,18 +76,18 @@ router.put('/', async (req, res) => {
 })
 
 // use checkJwt as middleware
-// DELETE /api/v1/fruits
+// DELETE /api/v1/pets
 router.delete('/:id', async (req, res) => {
   const id = Number(req.params.id)
   const auth0Id = req.user?.sub
   try {
-    const fruits = await db.deleteFruit(id, auth0Id)
-    res.json({ fruits })
+    const pets = await db.deletepet(id, auth0Id)
+    res.json({ pets })
   } catch (err) {
     console.error(err)
     if (err.message === 'Unauthorized') {
       return res.status(403).send(
-        'Unauthorized: Only the user who added the fruit may delete it'
+        'Unauthorized: Only the user who added the pet may delete it'
       )
     }
     res.status(500).send(err.message)

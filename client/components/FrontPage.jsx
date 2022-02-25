ae2f5a37aa0b8e60aa24d8de49cd23ae6c561340
ben-alefosio <ben_alefosio@hotmail.com>
2022-02-25 14:50:19 +1300

    arrows and pat/like buttons added

--- client/components/FrontPage.jsx
+++ client/components/FrontPage.jsx
@@ -2,29 +2,48 @@ import React from 'react'
 
 const Frontpage = () => {
   return (
-    <div className="dog-card-container">
-      <div className="dog-card">
-        <h1>B RKMATES</h1>
-        <img className="dog-card-nav" src='/images/nav-icon.png'/>
-        <img className="dog-card-img" src='/images/Caesar.jpg'/>
-        {/* <b>//Name :// </b> */}
-        <h2> Caesar </h2>
+    <>
+      <div className="dog-card-container">
+        <div className="dog-card">
+          <h1>BRKMATES</h1>
+          <img className="dog-card-nav" src='/images/nav-icon.png'/>
+          <img className="dog-card-img" src='/images/Caesar.jpg'/>
+          {/* <b>//Name :// </b> */}
+          <h1> Caesar </h1>
 
-        {/* <b>Age : </b> */}
-        <p> 5 months </p>
+          {/* <b>Age : </b> */}
+          <p> 5 months </p>
 
-        {/* <b>Breed : </b> */}
-        <p> American Pocket Bully </p>
+          {/* <b>Breed : </b> */}
+          <p> American Pocket Bully </p>
 
-        {/* <b>Description: </b> */}
-        <p> Little lover boy!!</p>
+          {/* <b>Description: </b> */}
+          <p> Little lover boy!!</p>
 
-        <div className="clear"></div>
+          <p> VIEW PROFILE... </p>
+
+          <div className="clear"></div>
+          <img className="leftArrow" src='/images/leftArrow.png'/>
+
+          <img className="rightArrow" src='/images/rightArrow.png'/>
+
+        </div>
       </div>
+
       <div>
+        <div className="leftSideCard">
+          <button>
+            <h3>SEND TREATS</h3>
+          </button>
+        </div>
 
+        <div className="rightSideCard">
+          <button>
+            <h3>SEND PATS</h3>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 

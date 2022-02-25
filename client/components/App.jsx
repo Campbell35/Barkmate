0ec0c975e14814e7d11483ae744bc55321539401
ben-alefosio <ben_alefosio@hotmail.com>
2022-02-25 14:59:12 +1300

    barkmates front page 90 percent

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Nav from './Nav'
-// import Fruits from './Fruits'
+import Fruits from './Fruits'
 import { cacheUser } from '../auth0-utils'
 import { useAuth0 } from '@auth0/auth0-react'
-// import ProfilePage from './ProfilePage'
+import ProfilePage from './ProfilePage'
 import FrontPage from './FrontPage'
-// import Register from './Register'
+import Register from './Register'
 import Chat from './Chat'
 
 function App () {
@@ -18,7 +18,7 @@ function App () {
       <Route path='/' component={Nav} />
       <Route path='/chat' component={Chat} />
       {/* <Route exact path='/' component={Fruits} /> */}
-      {/* <Route exact path='/profile' component={ProfilePage} /> */}
+      <Route exact path='/profile' component={ProfilePage} />
     </Router>
   )
 }

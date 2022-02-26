e6467c7c84f101f95412c9226186f1db1c0f1521
tarek-chakra <tarek.s.abouchakra@gmail.com>
2022-02-26 16:33:24 +1300

    liked pet state and welp state

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -8,15 +8,15 @@ module.exports = router
 // A route to direct you to the humnan page after you sign in
 // GET /api/v1/likes
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const humans = await db.getHumans()
-//     res.json({ humans })
-//   } catch (err) {
-//     console.error(err)
-//     res.status(500).send(err.message)
-//   }
-// })
+router.get('/', async (req, res) => {
+  try {
+    const likes = await db.getLikes()
+    res.json({ likes })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err.message)
+  }
+})
 
 router.post('/', async (req, res) => {
   console.log(req.body)

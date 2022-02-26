cf3eae82bd97877b679d3d34bb3b75b5cf323092
tarek-chakra <tarek.s.abouchakra@gmail.com>
2022-02-26 17:25:47 +1300

    adding a match function to the back end

--- server/db/matches.js
+++ server/db/matches.js
@@ -1,9 +1,15 @@
 const connection = require('./connection')
 
-function getMatches(db = connection) {
+function getMatches (db = connection) {
   return db('matches').select()
 }
 
+function addAMatch (match, db = connection) {
+  return db('matches').insert(match)
+    .then(() => null)
+}
+
 module.exports = {
-  getMatches
+  getMatches,
+  addAMatch
 }

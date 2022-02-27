aa21065d067d7bee877c22eeb1f8315768dfd2f6
Qian Han <kategreat1@hotmail.com>
2022-02-27 19:34:10 +1300

    fix css-styling

--- client/components/FrontPage.jsx
+++ client/components/FrontPage.jsx
@@ -29,7 +29,7 @@ const Frontpage = () => {
   }, [owner.token])
 
   async function clickHandleLike () {
-    await addLike(owner.id, Number(pet.owner_id))
+    await addLike(owner.id, Number(pet.owner_id), owner.token)
     setLiked(true)
   }
 
@@ -56,66 +56,53 @@ const Frontpage = () => {
             <img className="dog-card-nav" src='/images/nav-icon.png'/>
             <img className="dog-card-img" src={pet.images}/>
             {/* <b>//Name :// </b> */}
-            <h1> {pet.name} </h1>
+            <h3> {pet.name} </h3>
 
             {/* <b>Breed : </b> */}
             <p> {pet.breed} </p>
 
             {/* <b>Energy Levels: </b> */}
-            <p> {pet.energy_levels}</p>
+            {/* <p> Energy Level: pet.energy_levels} </p> */}
 
             {/* <b>Description: </b> */}
-            <p> Little lover boy!!</p>
+            {/* <p> Little lover boy!!</p> */}
 
-            <p> VIEW PROFILE... </p>
+            <p> VIEW PROFILE </p>
+
+            <div className="clear"></div>
+
+          </div>
+
+          <div className="container21">
             <div>
-              <h2>Match ME</h2>
+              <button className="button21">
+                <div className="icon21">
+                  <svg viewBox="0 0 16 16" className="bi bi-telegram" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+                  </svg>
+                </div>
+                <p>SEND PAT</p>
+              </button>
+            </div>
+            <div>
+
+              {/* <p>Match ME</p> */}
               {!owner.id
                 ? <p>loading</p>
                 : likes.find(like => {
                   return like.liked_human_id === Number(pet.owner_id)
                 }) || liked
-                  ? <p>liked!</p>
-                  : <img className='pawheart' src='/images/pawheart.png' onClick={clickHandleLike}/>
+                  ? <h1>liked!</h1>
+                  : <img className='pawheart2' src='/images/pawheart.png' onClick={clickHandleLike}/>
               }
-            </div>
-
-            <div className="clear"></div>
-
-          </div>
-          <div>
-            <div className="container21">
-              <div className="leftSideCard">
-                <button>
-                  <h3>SEND TREATS</h3>
-
-                </button>
-              </div>
-
-              <div>
-                <h3><img src="https://i.imgur.com/rHHDxoS.png"/> Like</h3>
-              </div>
-
-              <div className="rightSideCard">
-                <button>
-                  <h3>SEND PATS</h3>
-
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="container21">
-
-            <div>
-
-              <img className="rightArrow" src='/images/leftArrow.png' />
 
             </div>
             <div>
-
-              <img className="leftArrow" src='/images/rightArrow.png'onClick={clickHandleRight}/>
-
+              <button className="cssbuttons-io-button" onClick={clickHandleRight}> Next
+                <div className="icon">
+                  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+                </div>
+              </button>
             </div>
           </div>
         </div>
--- server/db/seeds/02-pets.js
+++ server/db/seeds/02-pets.js
@@ -2,12 +2,12 @@ exports.seed = (knex) => {
   return knex('pets').del()
     .then(() => {
       return knex('pets').insert([
-        { id: 1, name: 'Rocky', breed: 'husky', owner_id: 2, energy_levels: 4, images: 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg', pats: 9, treats: 7 },
-        { id: 2, name: 'Hugo', breed: 'mixed', owner_id: 1, energy_levels: 5, images: 'https://cdn.britannica.com/49/161649-050-3F458ECF/Bernese-mountain-dog-grass.jpg', pats: 7, treats: 9 },
-        { id: 3, name: 'Rascal', breed: 'pug', owner_id: 3, energy_levels: 2, images: 'https://miro.medium.com/max/1400/1*XBC9diIIlWIbbE8BVXgulA.jpeg', pats: 7, treats: 9 },
-        { id: 4, name: 'Tiny', breed: 'great dane', owner_id: 4, energy_levels: 3, images: 'https://media.npr.org/assets/img/2014/09/12/ap300370671383_wide-e907716e290a0b70af798f66abe5b7e4ef40d8dd.jpg', pats: 7, treats: 9 },
-        { id: 5, name: 'Gertrude', breed: 'collie', owner_id: 5, energy_levels: 1, images: 'https://www.akc.org/wp-content/uploads/2017/11/Collie-standing-in-a-field.jpg', pats: 7, treats: 9 }
-
+        { id: 1, name: 'Rocky', breed: 'Cavalier King Charles Spaniel', owner_id: 2, energy_levels: 4, images: 'http://cdn.akc.org/content/article-body-image/cavkingcharlessmalldogs.jpg', pats: 9, treats: 7 },
+        { id: 2, name: 'Hugo', breed: 'Yakutian Laika', owner_id: 1, energy_levels: 5, images: 'https://www.k9ofmine.com/wp-content/uploads/2021/02/yakutian-laika.jpg', pats: 7, treats: 9 },
+        { id: 3, name: 'Rascal', breed: 'husky', owner_id: 3, energy_levels: 2, images: 'https://www.desktopbackground.org/p/2011/06/01/212304_cute-baby-husky-wallpaper_2560x1600_h.jpg', pats: 7, treats: 9 },
+        { id: 4, name: 'Tiny', breed: 'pug', owner_id: 4, energy_levels: 3, images: 'https://www.allthingsdogs.com/wp-content/uploads/2020/02/Pug-Feature-678x381.jpg', pats: 7, treats: 9 },
+        { id: 5, name: 'Gertrude', breed: 'Norfolk Terrier', owner_id: 5, energy_levels: 1, images: 'http://cdn.akc.org/content/article-body-image/norfolkterriersmalldogs.jpg', pats: 7, treats: 9 },
+        { id: 6, name: 'Apple', breed: 'bulldog', owner_id: 5, energy_levels: 1, images: 'https://c4.wallpaperflare.com/wallpaper/606/740/844/best-friends-bulldog-cute-dog-photos-dog-images-wallpaper-preview.jpg', pats: 7, treats: 9 }
       ])
     })
 }

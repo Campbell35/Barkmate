cdf1bfcd517174ffc95c9b0b142bc3ee5c1dfab5
Kate Han <84889124+katethegreat1@users.noreply.github.com>
2022-03-02 11:23:34 +1300

    Update HumanProfile.jsx

--- client/components/Profile/HumanProfile.jsx
+++ client/components/Profile/HumanProfile.jsx
@@ -3,6 +3,14 @@ import { getPetsByOwner } from '../../api'
 import { useSelector } from 'react-redux'
 import Navigation from '../Navigation/Navigation'
 
+
+function handleAdd (e) {
+  e.preventDefault()
+  loginWithRedirect({
+    redirectUri: `${window.location.origin}/petprofile/create`
+  })
+}
+
 function HumanProfile () {
   const owner = useSelector(state => state.human)
   const [petArr, setPetArr] = useState([])
@@ -43,6 +51,11 @@ function HumanProfile () {
                     ENERGY LEVELS: {pet.energy_levels} <br></br>
                     TOTAL PATS: {pet.pats} <br></br>
                     MY QUOTE: {pet.quote}</span></div>
+                      
+                    <div>
+                     <button className='btnform' onClick={handleAdd}>Add a pet</button>
+                    </div>
+  
                   </li>
                 </div>
                 <br></br>

bcd4ccc07f93fd904c052c66cbd2deb7eb3708bb
ben-alefosio <ben_alefosio@hotmail.com>
2022-03-02 10:33:54 +1300

    dog loading icon add

--- client/components/Profile/HumanProfile.jsx
+++ client/components/Profile/HumanProfile.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { getPetsByOwner } from '../../api'
 import { useSelector } from 'react-redux'
 import Navigation from '../Navigation/Navigation'
+import WaitIndicator from '../WaitIndicator'
+const waiting = useSelector(state => state.waiting)
 
 function HumanProfile () {
   const owner = useSelector(state => state.human)
@@ -22,35 +24,38 @@ function HumanProfile () {
   }, [owner.token])
 
   if (owner.token) {
-    return (
-      <div className="dog-card-container">
-        <div className="dog-card">
-          <Navigation/>
-          <img className='logoimg' src='/images/Logo.png'/>
-          <p>{owner.name}&apos;s profile</p>
-          <h1>My dogs:</h1>
-          {petArr.map(pet => (
-            <>
-              <ul className="nobull">
-                <div className ="owner-pet-border">
-                  <li>
-                    <div className='round-img'>
-                      <img className ="owner-pet-image" src={pet.images}>
+    return (waiting
+      ? <WaitIndicator />
+      : <>
+        <div className="dog-card-container">
+          <div className="dog-card">
+            <Navigation/>
+            <img className='logoimg' src='/images/Logo.png'/>
+            <p>{owner.name}&apos;s profile</p>
+            <h1>My dogs:</h1>
+            {petArr.map(pet => (
+              <>
+                <ul className="nobull">
+                  <div className ="owner-pet-border">
+                    <li>
+                      <div className='round-img'>
+                        <img className ="owner-pet-image" src={pet.images}>
 
-                      </img></div>
-                    <div className='mypet-text'>
-                      {pet.name}<br></br><span className='subtitle-pets'>
+                        </img></div>
+                      <div className='mypet-text'>
+                        {pet.name}<br></br><span className='subtitle-pets'>
                     ENERGY LEVELS: {pet.energy_levels} <br></br>
                     TOTAL PATS: {pet.pats} <br></br>
                     MY QUOTE: {pet.quote}</span></div>
-                  </li>
-                </div>
-                <br></br>
-              </ul>
-            </>
-          ))}
+                    </li>
+                  </div>
+                  <br></br>
+                </ul>
+              </>
+            ))}
+          </div>
         </div>
-      </div>
+      </>
     )
   } else {
     return (<div> LOADING ... </div>)
